d5c8dddc3f20f5a30609113ceb383cc5d4c224ce
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-03-29 23:30:16 +0500

    used perlin noise to get generated terrains

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <ctime>
+#include "PerlinNoise.hpp"
 
 using namespace std;
 using namespace sf;
@@ -7,37 +9,74 @@ using namespace sf;
 int main()
 {
 
-    const int height = 500;
-    const int width = 500;
+    srand(time(NULL));
 
-    int size = 100;
+    const int height = 700;
+    const int width = 700;
 
     RenderWindow window(VideoMode(width, height), "Coexistence");
-    CircleShape shape(size);
-    shape.setPosition(Vector2f(((int)(width / 2)) - ((int)(size)),
-                               ((int)(height / 2)) - ((int)(size))));
 
-    shape.setFillColor(Color::Blue);
+    int x = 0;
 
-    int i = 0;
+    // Pixel Array
+    RectangleShape *screenPixels[height][width];
+
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            screenPixels[i][j] = new RectangleShape(Vector2f(1, 1));
+        }
+    }
 
     while (window.isOpen())
     {
+        const siv::PerlinNoise::seed_type seed = rand();
+        const siv::PerlinNoise perlin{seed};
 
         Event event;
         while (window.pollEvent(event))
         {
             if (event.type == Event::Closed)
+            {
                 window.close();
+            }
         }
-        i++;
-        cout << i << endl;
-        window.clear();
-        // shape.setScale(Vector2f(i + 100, i));
-        shape.setPosition(shape.getPosition().x + (int)(i / 200),
-                          shape.getPosition().y + (int)(i / 200));
 
-        window.draw(shape);
+        window.clear();
+
+        if (x < 500)
+        {
+            x++;
+        }
+        else
+        {
+            x = 0;
+        }
+
+        // Draw the screen
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+
+                const double noise = perlin.octave2D_01((i * 0.01), (j * 0.01), 4);
+                cout << "Noise: " << noise << endl;
+                if (noise > 0.3)
+                {
+                    screenPixels[i][j]->setFillColor(Color(255, 255, 255, 255));
+                }
+                else
+                {
+                    screenPixels[i][j]->setFillColor(Color(255, 255, 255, 0));
+                }
+
+                screenPixels[i][j]->setPosition(Vector2f(i, j));
+
+                window.draw(*screenPixels[i][j]);
+            }
+        }
+
         window.display();
     }
 
